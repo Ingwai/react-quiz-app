ea5f91d8e32ba9453df9ac80bc8aeed9de334968
Ingwai <ingwai@wp.pl>
2023-12-21 19:56:17 +0100

    initial app

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const QuestionTimer = ({ timeout, onTimeout, mode }) => {
+	const [remainingTime, setRemainingTime] = useState(timeout);
+
+	useEffect(() => {
+		const timer = setTimeout(onTimeout, timeout);
+
+		return () => clearTimeout(timer);
+	}, [onTimeout, timeout]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+		}, 100);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return <progress id='question-time' value={remainingTime} max={timeout} className={mode}></progress>;
+};
+
+export default QuestionTimer;
